c11a398b1ccfd5a40b151c7cc1a8306dfe10dad5
jackieblin <jackieblin@tencent.com>
2019-02-21 17:26:20 +0800

    136.single-number solved

---
+++ 136.single-number/index.js
@@ -0,0 +1,21 @@
+
+/**
+ * 可以采用 异或的方式解决，厉害了：singleNumber = nums=> nums.reduce((r, x)=>r^x, 0)
+ * 2∗(a+b+c)−(a+a+b+b+c)=c 数学方式，强无敌
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    var map = {};
+    for (var i = 0; i < nums.length; i++) {
+        if (!map[nums[i]]) {
+            map[nums[i]] = true;
+        } else {
+            delete map[nums[i]];
+        }
+    }
+    
+    for(var key in map) {
+        return key
+    }
+};
