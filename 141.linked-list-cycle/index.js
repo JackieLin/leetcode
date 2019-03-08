e6e171ea6fd09a8e815efb8f9ce7aeecc623a6a8
JackieLin <dashi_lin@163.com>
2019-03-08 17:21:46 +0800

    141.linked-list-cycle solved

---
+++ 141.linked-list-cycle/index.js
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=141 lang=javascript
+ *
+ * [141] Linked List Cycle
+ *
+ * https://leetcode.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (35.83%)
+ * Total Accepted:    366.4K
+ * Total Submissions: 1M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, determine if it has a cycle in it.
+ * 
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1], pos = -1
+ * Output: false
+ * Explanation: There is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Follow up:
+ * 
+ * Can you solve it using O(1) (i.e. constant) memory?
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    var curr = head;
+    var map = new WeakMap()
+    while(curr) {
+        if (map.get(curr)) return true;
+        map.set(curr, true);
+        curr = curr.next;
+    }
+
+    return false;
+};
+
