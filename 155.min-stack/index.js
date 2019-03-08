e839b1d7dc57fa64d57bac700bf38eb1696f360a
JackieLin <dashi_lin@163.com>
2019-03-08 17:39:22 +0800

    155.min-stack solved

---
+++ 155.min-stack/index.js
@@ -0,0 +1,104 @@
+/*
+ * @lc app=leetcode id=155 lang=javascript
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (35.75%)
+ * Total Accepted:    271.7K
+ * Total Submissions: 760K
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * 
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * 
+ * push(x) -- Push element x onto stack.
+ * 
+ * 
+ * pop() -- Removes the element on top of the stack.
+ * 
+ * 
+ * top() -- Get the top element.
+ * 
+ * 
+ * getMin() -- Retrieve the minimum element in the stack.
+ * 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ * 
+ * 
+ */
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    this.stack = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    this.stack.push(x);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    if (!this.stack.length) {
+        return null;
+    } else {
+        return this.stack[this.stack.length - 1];
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    var min;
+    if (this.stack.length) {
+        min = this.stack[0];
+        for(var i = 1; i < this.stack.length; i++) {
+            if (min > this.stack[i]) {
+                min = this.stack[i];
+            }
+        }
+    }
+
+    return min;
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = Object.create(MinStack).createNew()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
