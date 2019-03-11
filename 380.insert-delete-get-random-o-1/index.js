d1b4277d1cae4b8d1f84a4c45e0c986ac8134a92
JackieLin <dashi_lin@163.com>
2019-03-11 20:01:08 +0800

    380.insert-delete-get-random-o-1 solved

---
+++ 380.insert-delete-get-random-o-1/index.js
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode id=380 lang=javascript
+ *
+ * [380] Insert Delete GetRandom O(1)
+ *
+ * https://leetcode.com/problems/insert-delete-getrandom-o1/description/
+ *
+ * algorithms
+ * Medium (42.05%)
+ * Total Accepted:    100.5K
+ * Total Submissions: 238.9K
+ * Testcase Example:  '["RandomizedSet","insert","remove","insert","getRandom","remove","insert","getRandom"]\n[[],[1],[2],[2],[],[1],[2],[]]'
+ *
+ * Design a data structure that supports all following operations in average
+ * O(1) time.
+ * 
+ * 
+ * 
+ * insert(val): Inserts an item val to the set if not already present.
+ * remove(val): Removes an item val from the set if present.
+ * getRandom: Returns a random element from current set of elements. Each
+ * element must have the same probability of being returned.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * // Init an empty set.
+ * RandomizedSet randomSet = new RandomizedSet();
+ * 
+ * // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+ * randomSet.insert(1);
+ * 
+ * // Returns false as 2 does not exist in the set.
+ * randomSet.remove(2);
+ * 
+ * // Inserts 2 to the set, returns true. Set now contains [1,2].
+ * randomSet.insert(2);
+ * 
+ * // getRandom should return either 1 or 2 randomly.
+ * randomSet.getRandom();
+ * 
+ * // Removes 1 from the set, returns true. Set now contains [2].
+ * randomSet.remove(1);
+ * 
+ * // 2 was already in the set, so return false.
+ * randomSet.insert(2);
+ * 
+ * // Since 2 is the only number in the set, getRandom always return 2.
+ * randomSet.getRandom();
+ * 
+ * 
+ */
+/**
+ * Initialize your data structure here.
+ */
+var RandomizedSet = function() {
+    this.set = {};
+};
+
+/**
+ * Inserts a value to the set. Returns true if the set did not already contain the specified element. 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+    if (this.set[val] === undefined) {
+        this.set[val] = true;
+        return true;
+    }
+
+    return false
+};
+
+/**
+ * Removes a value from the set. Returns true if the set contained the specified element. 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+    if (this.set[val] !== undefined) {
+        delete this.set[val];
+        return true;
+    }
+
+    return false
+};
+
+/**
+ * Get a random element from the set.
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+    var data = Object.keys(this.set)
+    return data[parseInt(Math.random() * data.length)];
+};
+
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = Object.create(RandomizedSet).createNew()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
+
