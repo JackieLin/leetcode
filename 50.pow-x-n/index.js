c33a0c5a6572d42a29d1a9a58b4b292786f8df65
JackieLin <dashi_lin@163.com>
2019-03-29 17:30:49 +0800

    50.pow-x-n solved

---
+++ 50.pow-x-n/index.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=50 lang=javascript
+ *
+ * [50] Pow(x, n)
+ *
+ * https://leetcode.com/problems/powx-n/description/
+ *
+ * algorithms
+ * Medium (27.71%)
+ * Total Accepted:    300.8K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '2.00000\n10'
+ *
+ * Implement pow(x, n), which calculates x raised to the power n (x^n).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 2.00000, 10
+ * Output: 1024.00000
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 2.10000, 3
+ * Output: 9.26100
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 2.00000, -2
+ * Output: 0.25000
+ * Explanation: 2^-2 = 1/2^2 = 1/4 = 0.25
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * -100.0 < x < 100.0
+ * n is a 32-bit signed integer, within the range [−2^31, 2^31 − 1]
+ * 
+ * 
+ */
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    return Math.pow(x, n);
+};
+
