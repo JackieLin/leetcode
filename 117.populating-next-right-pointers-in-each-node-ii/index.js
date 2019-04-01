40213a6addfd41ef78dc4f4b2f10c7dd634ca188
JackieLin <dashi_lin@163.com>
2019-04-01 16:19:38 +0800

    117.populating-next-right-pointers-in-each-node-ii solved

---
+++ 117.populating-next-right-pointers-in-each-node-ii/index.js
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode id=117 lang=javascript
+ *
+ * [117] Populating Next Right Pointers in Each Node II
+ *
+ * 注意，可以利用已经构建完成的 next，直接找到下一个节点
+ * 
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/description/
+ *
+ * algorithms
+ * Medium (33.80%)
+ * Total Accepted:    174.6K
+ * Total Submissions: 516.4K
+ * Testcase Example:  '{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}'
+ *
+ * Given a binary tree
+ * 
+ * 
+ * struct Node {
+ * ⁠ int val;
+ * ⁠ Node *left;
+ * ⁠ Node *right;
+ * ⁠ Node *next;
+ * }
+ * 
+ * 
+ * Populate each next pointer to point to its next right node. If there is no
+ * next right node, the next pointer should be set to NULL.
+ * 
+ * Initially, all next pointers are set to NULL.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * 
+ * 
+ * Input:
+ * {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+ * 
+ * Output:
+ * {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":null,"right":null,"val":7},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"6","left":null,"next":null,"right":{"$ref":"5"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"6"},"val":1}
+ * 
+ * Explanation: Given the above binary tree (Figure A), your function should
+ * populate each next pointer to point to its next right node, just like in
+ * Figure B.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * You may only use constant extra space.
+ * Recursive approach is fine, implicit stack space does not count as extra
+ * space for this problem.
+ * 
+ */
+/**
+ * // Definition for a Node.
+ * function Node(val,left,right,next) {
+ *    this.val = val;
+ *    this.left = left;
+ *    this.right = right;
+ *    this.next = next;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    if (!root) return root;
+    var queue = [{
+        node: root,
+        level: 0
+    }];
+
+    while(queue.length) {
+        var item = queue.shift();
+
+        if (queue.length && queue[0].level === item.level) {
+            // console.log(queue[0])
+            // console.log('====')
+            item.node.next = queue[0].node;
+        }
+
+        if (item.node.left) {
+            queue.push({
+                node: item.node.left,
+                level: item.level + 1
+            })
+        }
+
+        if (item.node.right) {
+            queue.push({
+                node: item.node.right,
+                level: item.level + 1
+            })
+        }
+    }
+
+    return root;
+};
+
