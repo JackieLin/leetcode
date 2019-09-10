55e2cc67a562a7745dc2c51973774aa8e4919990
jackieLin <dashi_lin@163.com>
2019-09-11 00:01:45 +0800

    399.evaluate-division unsolved

--- 399.evaluate-division.js
+++ 399.evaluate-division.js
@@ -1,8 +1,16 @@
 /*
  * @lc app=leetcode id=399 lang=javascript
- *
+ * 
  * [399] Evaluate Division
  */
+
+// calcEquation([ ["a", "b"], ["b", "c"] ], [2.0, 3.0], [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ])
+// calcEquation([["a","aa"]], [9.0], [["aa","a"],["aa","aa"]])
+
+// [["x1","x2"],["x2","x3"],["x3","x4"],["x4","x5"]]
+// [3.0,4.0,5.0,6.0]
+// [["x1","x5"],["x5","x2"],["x2","x4"],["x2","x2"],["x2","x9"],["x9","x9"]]
+// [360.0,0.00833,20.0,1.0,-1.0,-1.0]
 /**
  * @param {string[][]} equations
  * @param {number[]} values
@@ -15,13 +23,15 @@ var calcEquation = function (equations, values, queries) {
 
     for (var i = 0; i < equations.length; i++) {
         var item = equations[i]
-        map[`${item[0]}${item[1]}`] = values[i]
-        if (!map[`${item[1]}${item[0]}`]) {
-            map[`${item[1]}${item[0]}`] = 1 / values[i];
+        map[`${item[0]}-${item[1]}`] = values[i]
+        if (!map[`${item[1]}-${item[0]}`]) {
+            map[`${item[1]}-${item[0]}`] = 1 / values[i];
         }
+        map[`${item[0]}-${item[0]}`] = 1;
+        map[`${item[1]}-${item[1]}`] = 1;
     }
 
-    // debugger;
+    // debugger; 
     while (result.length > 1) {
         var left = []
         for (var i = 0; i < result.length - 1; i++) {
@@ -37,20 +47,20 @@ var calcEquation = function (equations, values, queries) {
                 if (Object.keys(itemMap) === 4) continue;
 
                 if (iItem[1] === jItem[0]) {
-                    map[`${iItem[0]}${jItem[1]}`] = values[i] * values[j]
-                    map[`${jItem[1]}${iItem[0]}`] = 1 / map[`${iItem[0]}${jItem[1]}`]
+                    map[`${iItem[0]}-${jItem[1]}`] = values[i] * values[j]
+                    map[`${jItem[1]}-${iItem[0]}`] = 1 / map[`${iItem[0]}-${jItem[1]}`]
                     left.push([iItem[0], jItem[1]])
                 } else if (iItem[1] === jItem[1]) {
-                    map[`${iItem[0]}${jItem[0]}`] = values[i] * (1 / values[j])
-                    map[`${jItem[0]}${iItem[0]}`] = 1 / map[`${iItem[0]}${jItem[0]}`]
+                    map[`${iItem[0]}-${jItem[0]}`] = values[i] * (1 / values[j])
+                    map[`${jItem[0]}-${iItem[0]}`] = 1 / map[`${iItem[0]}-${jItem[0]}`]
                     left.push([iItem[0], jItem[0]]);
                 } else if (iItem[0] === jItem[0]) {
-                    map[`${jItem[1]}${iItem[1]}`] = values[i] * (1 / values[j])
-                    map[`${iItem[1]}${jItem[1]}`] = 1 / map[`${jItem[1]}${iItem[1]}`]
+                    map[`${jItem[1]}-${iItem[1]}`] = values[i] * (1 / values[j])
+                    map[`${iItem[1]}-${jItem[1]}`] = 1 / map[`${jItem[1]}-${iItem[1]}`]
                     left.push([jItem[1], iItem[1]])
                 } else if (iItem[0] === jItem[1]) {
-                    map[`${jItem[0]}${iItem[1]}`] = values[i] * values[j]
-                    map[`${iItem[1]}${jItem[0]}`] = 1 / map[`${jItem[0]}${iItem[1]}`]
+                    map[`${jItem[0]}-${iItem[1]}`] = values[i] * values[j]
+                    map[`${iItem[1]}-${jItem[0]}`] = 1 / map[`${jItem[0]}-${iItem[1]}`]
                     left.push([jItem[0], iItem[1]])
                 }
             }
@@ -60,8 +70,8 @@ var calcEquation = function (equations, values, queries) {
 
     var data = []
     for(var i = 0; i < queries.length; i++) {
-        if (map[`${queries[i][0]}${queries[i][1]}`]) {
-            data.push(map[`${queries[i][0]}${queries[i][1]}`])
+        if (map[`${queries[i][0]}-${queries[i][1]}`]) {
+            data.push(map[`${queries[i][0]}-${queries[i][1]}`])
         } else {
             data.push(-1.0)
         }
